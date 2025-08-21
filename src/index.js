bf10b3a2bd699369267ec6639cbdb8e909963610
Abhinav Bajpai <Abhinav123bajpai@gmail.com>
2025-08-21 09:54:42 +0530

    fix 2

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 const fs = require('fs');
 
-const text = fs.readFileSync("input.txt", 'utf-8');
+const text = fs.readFileSync("src/input.txt", 'utf-8');
 
 const arr = text.split("\n");
 let sum = 0;
 for(let x of arr) {
   sum+=Number(x.split(" ")[1]);
 }
-fs.writeFileSync("output.txt", sum+"");
+fs.writeFileSync("src/output.txt", sum+"");
